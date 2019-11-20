4dbfeca351b67856837afdd54ebada5cfcde4f3f
jordondoug2019 <jdouglas2015@gmail.com>
2019-11-20 09:01:20 -0600

    React Comps

---
+++ mmo-client/src/comps/FightComp.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class FightComp extends Component
+{
+    render()
+    {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default FightComp;
---
+++ mmo-client/src/comps/HomeComp.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class HomeComp extends Component
+{
+    render()
+    {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default HomeComp;
